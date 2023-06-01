56ad1348e2c3b3baadb6d295f7e956031c3ee889
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-06-01 13:36:57 +0600

    button added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Login.css';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is Login</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor='email'>Email</label>
+                    <input type='email' name='email' required></input>
+                </div>
+                <div className="form-control">
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' name='password' required></input>
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p>New to Ema John, <Link to={'/signup'}>Create a new account</Link></p>
         </div>
     );
 };
